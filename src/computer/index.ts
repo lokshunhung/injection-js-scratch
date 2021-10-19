15d8183b2f73496cd8be637f2727ec2a5e4a6ead
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-10-19 15:59:17 +0800

    add assertions

--- src/computer/index.ts
+++ src/computer/index.ts
@@ -1,12 +1,13 @@
 import "reflect-metadata";
 
+import { strict as assert } from "assert";
 import { ReflectiveInjector } from "injection-js";
 import { baseProvider } from "./base-provider";
 import { Macbook } from "./macbook/macbook";
 import { macbookProviders } from "./macbook/macbook-module";
+import { ConsoleOutput } from "./output/console-output";
 import { PC } from "./pc/pc";
 import { pcProvider } from "./pc/pc-modulet";
-import { ConsoleOutput } from "./output/console-output";
 
 export function main(): void {
     const base = ReflectiveInjector.resolveAndCreate(baseProvider);
@@ -23,6 +24,16 @@ export function main(): void {
     const pc: PC = pcInjector.get(PC);
     consoleOutput.println(pc.info());
     pc.boot();
+
+    // Check that there's only one instance of ConsoleOutput
+    assert(
+        Object.is(consoleOutput, base.get(ConsoleOutput)),
+        "expect instance of ConsoleOutput is cached in base"
+    );
+    assert(
+        Object.is(consoleOutput, macbookInjector.get(ConsoleOutput)),
+        "expect instance of ConsoleOutput from macbookInjector is inherited from base"
+    );
 }
 
 if (require.main === module) {
